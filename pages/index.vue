33650b1a12f88dda8442128c8524ea750e05da50
unknown <balloigor66@gmail.com>
2022-12-07 12:26:48 +0000

    v

--- pages/index.vue
+++ pages/index.vue
@@ -101,7 +101,6 @@
             <v-menu
               ref="depart_menu"
               v-model="depart_menu"
-              :close-on-content-click="false"
               :return-value.sync="date"
               transition="scale-transition"
               offset-y
@@ -123,28 +122,12 @@
                 no-title
                 scrollable
               >
-                <v-spacer></v-spacer>
-                <v-btn
-                  text
-                  color="primary"
-                  @click="depart_menu = false"
-                >
-                  Cancel
-                </v-btn>
-                <v-btn
-                  text
-                  color="primary"
-                  @click="$refs.depart_menu.save(date)"
-                >
-                  OK
-                </v-btn>
               </v-date-picker>
             </v-menu>
 
             <v-menu
               ref="menu"
               v-model="menu"
-              :close-on-content-click="false"
               :return-value.sync="date"
               transition="scale-transition"
               offset-y
@@ -166,21 +149,6 @@
                 no-title
                 scrollable
               >
-                <v-spacer></v-spacer>
-                <v-btn
-                  text
-                  color="primary"
-                  @click="menu = false"
-                >
-                  Cancel
-                </v-btn>
-                <v-btn
-                  text
-                  color="primary"
-                  @click="$refs.menu.save(reservationForm.comeback_date)"
-                >
-                  OK
-                </v-btn>
               </v-date-picker>
             </v-menu>
 
