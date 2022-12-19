7dfeb03212c621dc8621cd9f8280b0dd6a2c0b0e
ALUtech166 <s.tassiga@alustudent.com>
2022-12-19 11:47:04 +0000

    m

--- components/Footer.vue
+++ components/Footer.vue
@@ -25,14 +25,6 @@
               </path>
             </svg>
           </a>
-          <a href="#!" class="tw-text-white tw-mr-6 ">
-            <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google"
-                 class="tw-w-3.5" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512">
-              <path fill="currentColor"
-                    d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z">
-              </path>
-            </svg>
-          </a>
           <a href="https://www.instagram.com/manglam_tours_and_travels_228/" class="tw-text-white tw-mr-6 ">
             <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="instagram"
                  class="tw-w-3.5" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
@@ -41,7 +33,7 @@
               </path>
             </svg>
           </a>
-          <a href="#!" class="tw-text-white tw-mr-6 ">
+          <a href="https://www.linkedin.com/company/manglamtours228/" class="tw-text-white tw-mr-6 ">
             <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="linkedin-in"
                  class="tw-w-3.5" role="img" xmlns="http://www.w3.org/2000/svg"
                  viewBox="0 0 448 512">
