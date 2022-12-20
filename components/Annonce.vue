0f6ffc5f891a7f3988ea7beaf7613149225a7df4
ALUtech166 <s.tassiga@alustudent.com>
2022-12-20 14:44:45 +0000

    update

---
+++ components/Annonce.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <!-- Our partners section   -->
+    <div class="tw-bg-blue-900 tw-opacity-80 tw-p-4 tw-text-white">
+        <vue-marquee style="height:20px" :showProgress="false">
+          <vue-marquee-slide v-for="i in 10" :key="i">
+            <span style="padding: 35px"> Bienvenue à Manglam. on vous souhaite un trés bon voyage</span>
+          </vue-marquee-slide>
+        </vue-marquee>
+      </div>
+  </div>
+</template>
+
+<script>
+import {Carousel, Slide} from 'vue-carousel';
+
+export default {
+  components: {
+    Carousel,
+    Slide
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
