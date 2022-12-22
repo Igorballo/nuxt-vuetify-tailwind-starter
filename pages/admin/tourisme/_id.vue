c2554d64f1d68def72a303eee5f327aea46a3123
ALUtech166 <s.tassiga@alustudent.com>
2022-12-22 15:31:40 +0000

    update

--- pages/admin/tourisme/_id.vue
+++ pages/admin/tourisme/_id.vue
@@ -152,7 +152,50 @@
           </v-card-title>
           <v-card-text class="tw-px-4 tw-flex tw-flex-col tw-gap-4">
             <v-card>
-              <v-file-input outlined v-model="selectedFile" accept="application/pdf" multiple @change="handleFileChange" />
+               <input outlined label="Photos" placeholder="Les Photos de l'Hôtel" multiple
+                               @change="handleUploadChange" id="images-upload" type="file" accept="image/*"
+                               style="display:none" multiple hidden/>
+                        <div
+                          class="tw-my-4 tw-flex tw-justify-center tw-h-[fit-content] tw-border tw-rounded tw-bg-white">
+                          <div @click="triggerUpload" class="tw-w-full tw-bg-slate-100 tw-mx-1 tw-my-1 tw-rounded-md">
+                            <div v-if="media.length === 0"
+                                 class="tw-grid tw-justify-items-center tw-py-2 tw-my-2 hover:tw-cursor-pointer">
+                              <div class="tw-bg-slate-200 tw-py-2 tw-rounded-full tw-px-2">
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  class="h-6 w-6"
+                                  fill="none"
+                                  viewBox="0 0 24 24"
+                                  stroke="currentColor"
+                                  stroke-width="2"
+                                >
+                                  <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+                                  />
+                                </svg>
+                              </div>
+                              <p class="tw-text-md tw-font-weight">
+                                Ajouter des photos ou vidéos
+                              </p>
+                              <p class="tw-text-sm">
+                                ou faites glisser-deposer
+                              </p>
+                            </div>
+
+                            <div class="tw-flex tw-gap-2 tw-flex-wrap tw-p-4" v-else>
+                              <div class="tw-relative tw-p-2 tw-rounded-md tw-border tw-bg-white"
+                                   v-for="(image, index) in media">
+                                    <span @click.stop="removeImageUploaded(index)" class="tw-absolute tw-cursor-pointer tw-shadow-md tw-right-4 hover:tw-bg-red-700 tw-top-4 tw-rounded-full tw-bg-red-500 tw-text-white tw-text-sm tw-h-6 tw-w-6 tw-text-center">
+                                      x
+                                  </span>
+                                <img class="tw-w-32 tw-h-32 tw-rounded-lg" :src="image.url">
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
               <v-card-actions class="tw-mb-6" v-if="">
                 <v-btn color="primary" @click="sendSupplyToClient()" block>Envoyer cette offre au client</v-btn>
               </v-card-actions>
@@ -193,6 +236,7 @@ export default {
           href: '',
         },
       ],
+      media: [],
       selectedFile: null,
       demande_tourisme_loading: false,
       demande_tourisme: null,
@@ -213,9 +257,27 @@ export default {
           console.log(error)
       })
     },
-  watch: {
+ triggerUpload() {
+      let inputImage = document.getElementById("images-upload")
+      inputImage.click()
+    },
+    removeImageUploaded(index) {
+      this.media.splice(index, 1)
+    },
+    async handleUploadChange($event) {
+      let files = $event.target.files
+      for (var i = 0; i < files.length; i++) {
+        let formData = new FormData
+        let url = URL.createObjectURL(files[i])
+        formData.set('image', files[i])
 
-  },
+        let mediadata = {url: url, size: files[i].size, type: files[i].type, file: files[i]};
+        console.log(mediadata)
+
+        this.media.push(mediadata);
+
+      }
+    },
 }
 }
 </script>
